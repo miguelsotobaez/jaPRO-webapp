999e46264833287709d813950b3658ba1652307d
Miguel Soto Baez <miguelsotobaez@gmail.com>
2017-11-16 17:19:02 -0300

    Default title and database route
    Changed default title, database conection are only readeble.

--- inc/config.inc.php
+++ inc/config.inc.php
@@ -1,8 +1,9 @@
 <?php
-define("TITLE","JKLATAM - JAPRO");
-define("NAME","JKLATAM");
+define("TITLE","jaPRO - WebApp");
+define("NAME","jaPRO");
 define("VERSION","v.0.2");
-define("DATABASE_ROUTE","C:\Repositorios\jaPRO-webapp\japro\data.db");
+define("DATABASE_ROUTE","C:\Repositorios\jaPRO-webapp\japro\data1.db");
 define("COLOR","#ffffff,#000000");
 define("LANG","EN");
+//This is miguelsotobaez database location  define("DATABASE_ROUTE","C:\Repositorios\jaPRO-webapp\japro\data1.db");
 ?>

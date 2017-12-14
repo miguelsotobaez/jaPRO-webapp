f578435977a164dd4bf6ab33cc9791ba91d1120a
david <videoprofess@gmail.com>
2017-12-14 17:35:57 -0500

    implement last_update time

--- ajax/updateDB.php
+++ ajax/updateDB.php
@@ -3,66 +3,63 @@ require_once('../inc/db_connection.inc.php');
 //require_once('../inc/session.inc.php');
 
 $option = null;
-if (isset($_POST['option'])) {
-	$option = $_POST["option"];
+if (isset($_GET['option'])) {
+	$option = $_GET["option"];
 }
 
 switch ($option) {
 	case "races":
 		$last_update = $db->querySingle("SELECT last_update FROM updates WHERE type = 'races'");
+		$time = time();
+		if ($last_update + 60 > $time) { //If last update was within 1 minute, exit.
+			break;
+		}
 
-		//If last update was within 1 minute, exit.
-
-		//Get data from gameserver/update.php POST type = races, last_update = last_update
-		$json = GetStats($option, $last_update);
+		$json = GetStats("races", $last_update); //Get data from gameserver
 		//echo $json;
 
 		//Insert data into races
 
 		//Cleanup races (delete duplicate course/style/usernames with higher duration_ms... or lower end_time ? )
 
-		//last_update = getTime
-
 		$stmt = $db->prepare("UPDATE updates SET last_update = :last_update WHERE type = 'races'");
-		$stmt->bindValue(':last_update', $last_update);
+		$stmt->bindValue(':last_update', $time);
 		$result = $stmt->execute();
 	break;
 
 	case "duels":
 		$last_update = $db->querySingle("SELECT last_update FROM updates WHERE type = 'duels'");
+		$time = time();
+		if ($last_update + 60 > $time) { //If last update was within 1 minute, exit.
+			break;
+		}
 
-		//If last update was within 1 minute, exit.
-
-		//Get data from gameserver/update.php POST type = duels, last_update = last_update
-		$json = GetStats($option, $last_update);
+		$json = GetStats("duels", $last_update); //Get data from gameserver
 
 		//Insert data into duels
 
 		//No need for cleanup, but it would just be deleting actual duplicate entries
 
-		//last_update = getTime
-
 		$stmt = $db->prepare("UPDATE updates SET last_update = :last_update WHERE type = 'duels'");
-		$stmt->bindValue(':last_update', $last_update);
+		$stmt->bindValue(':last_update', $time);
 		$result = $stmt->execute();
 	break;
 
 	case "accounts":
 		$last_update = $db->querySingle("SELECT last_update FROM updates WHERE type = 'accounts'");
+		$time = time();
+		if ($last_update + 60 > $time) { //If last update was within 1 minute, exit.
+			break;
+		}
 
-		//If last update was within 1 minute, exit.
-
-		//Get data from gameserver/update.php POST type = accounts, last_update = last_update
-		$json = GetStats($option, $last_update);
+		$json = GetStats("accounts", $last_update); //Get data from gameserver
 
 		//Insert data into accounts
 
 		//Delete duplicate usernames with lower lastlogin ? or should we just refresh this table completely 
 
-		//last_update = getTime
-
 		$stmt = $db->prepare("UPDATE updates SET last_update = :last_update WHERE type = 'accounts'");
-		$stmt->bindValue(':last_update', $last_update);
+		$stmt->bindValue(':last_update', $time);
 		$result = $stmt->execute();
 	break;
 }
